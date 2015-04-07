b6185ca7f490c9cbfd266bdb83edc182df63a7a2
Aalaysia Stepney <1stepneyaalaysia@gmail.com>
2015-04-07 15:01:51 -0700

    7guiugp87o5t 67t8po 7t;

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,6 +1,6 @@
 game.PlayerEntity = me.Entity.extend({
     init: function(x, y, settings){
-        this.setSuper();
+        this.setSuper(x, y);
         this.setPlayerTimer();
         this.setAttributes();
         this.type = "PlayerEntity";
@@ -13,7 +13,7 @@ game.PlayerEntity = me.Entity.extend({
     this.renderable.setCurrentAnimation("idle");
 },
 
-setSuper: function(){
+setSuper: function(x, y, settings){
             this._super(me.Entity, 'init', [x, y, {
                 image: "player",
                 width: 64,
@@ -26,7 +26,7 @@ setSuper: function(){
         }]);
     },
 
-setPlayerTimers: function(){
+setPlayerTimer: function(){
     this.now = new Date().getTime();
     this.lastHit = this.now;
     this.lastAttack = new Date().getTime(); //Haven't used this
@@ -42,6 +42,7 @@ setFlags: function(){
      //Keeps track of which direction your character is going
     this.facing = "right";
     this.dead = false;
+    this.attacking = false;
 },
 
 addAnimation: function(){
@@ -53,13 +54,60 @@ addAnimation: function(){
 
     update: function(delta){
         this.now = new Date() .getTime();
+        this.dead = this.checkIfDead();
+        this.checkKeyPressesAndMoves();
+        this.setAnimation();
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
+        this.body.update(delta);
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    checkIfDead: function() {
+        if (this.health <= 0) {
+            return true;
+        }
+        return false;
+    },
+    
+    checkKeyPressesAndMoves: function(){
+        if(me.input.isKeyPressed("right")){
+           this.moveRight();
+        }else if(me.input.isKeyPressed("left")){
+            this.moveLeft();
+        }else{
+            this.body.vel.x = 0;
+        }
         
-        this.dead = checkIfDead();
+        if(me.input.isKeyPressed("jump") && !this.body.jumping && !this.body.falling){
+           this.jump();
+        }
         
-        this.checkKeyPressesAndMove();
-
-        
-             if(me.input.isKeyPressed("attack")){
+        this.attacking = me.input.isKeyPressed("attack");
+    },
+    
+    moveRight: function(){
+         //adds the position of my x by the velovity defined above in
+            //setVelocity() and multiplying it by me.timer.tick.
+            //me.timer.tick makes the movement look smooth
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+            this.facing = "right";
+            this.flipX(true);
+    },
+    
+    moveLeft: function(){
+        this.facing = "left";
+            this.body.vel.x -=this.body.accel.x * me.timer.tick;
+            this.flipX(false);
+    },
+    
+    jump: function(){
+         this.body.jumping = true;
+         this.body.vel.y -=this.body.accel.y * me.timer.tick;
+    },
+    
+    setAnimation: function(){
+             if(this.attacking){
             if(!this.renderable.isCurrentAnimation("attack")){
                 //Sets the current animation to attack and once that is over
                 //goes back to idle animation
@@ -79,48 +127,6 @@ addAnimation: function(){
     }else if(!this.renderable.isCurrentAnimation("attack")){
         this.renderable.setCurrentAnimation("idle");
     }
-        
-        me.collision.check(this, true, this.collideHandler.bind(this), true);
-        this.body.update(delta);
-        
-        this._super(me.Entity, "update", [delta]);
-        return true;
-    },
-    
-    checkIfDead: function(){
-         if(this.health <= 0){
-            return true;
-        }
-        return false;
-    },
-    
-    checkKeyPressesAndMoves: function(){
-        if(me.input.isKeyPressed("right")){
-           this.moveRight();
-        }else if(me.input.isKeyPressed("left")){
-            this.moveLeft();
-        }else{
-            this.body.vel.x = 0;
-        }
-        
-        if(me.input.isKeyPressed("jump") && !this.body.jumping && !this.body.falling){
-           this.jump();
-        }
-    },
-    
-    moveRight: function(){
-         //adds the position of my x by the velovity defined above in
-            //setVelocity() and multiplying it by me.timer.tick.
-            //me.timer.tick makes the movement look smooth
-            this.body.vel.x += this.body.accel.x * me.timer.tick;
-            this.facing = "right";
-            this.flipX(true);
-    },
-    
-    moveLeft: function(){
-        this.facing = "left";
-            this.body.vel.x -=this.body.accel.x * me.timer.tick;
-            this.flipX(false);
     },
     
     loseHealth: function(damage){
