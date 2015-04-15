f9c3854c3bce865ea4bbe0b5df197e2d41789387
Aalaysia Stepney <1stepneyaalaysia@gmail.com>
2015-04-15 14:59:41 -0700

    gjh

--- js/game.js
+++ js/game.js
@@ -51,7 +51,7 @@ var game = {
         
        me.save.add({exp: 0, exp1: 0, exp2: 0, exp3: 0, exp4: 0}); 
 
-
+       me.state.SPENDEXP = 112;
        
 	// Initialize the audio.
 	me.audio.init("mp3,ogg");
@@ -74,12 +74,13 @@ var game = {
             me.pool.register("EnemyCreep", game.EnemyCreep, true);
             me.pool.register("GameTimerManager", game.GameTimerManager);
             me.pool.register("HeroDeathManager", game.HeroDeathManager);
-             me.pool.register("ExperienceManager", game.ExperienceManager);
+            me.pool.register("ExperienceManager", game.ExperienceManager);
             
-		me.state.set(me.state.MENU, new game.TitleScreen());
-		me.state.set(me.state.PLAY, new game.PlayScreen());
+            me.state.set(me.state.MENU, new game.TitleScreen());
+            me.state.set(me.state.PLAY, new game.PlayScreen());
+            me.state.set(me.state.SPENDEXP, new game.SpendExp());
 
 		// Start the game.
-		me.state.change(me.state.MENU);
+            me.state.change(me.state.MENU);
 	}
 };
