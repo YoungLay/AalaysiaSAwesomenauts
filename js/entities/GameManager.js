e41f31b06436731105278ae90480259c641d2774
Aalaysia Stepney <1stepneyaalaysia@gmail.com>
2015-04-27 15:02:48 -0700

    bnjhn

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -94,6 +94,8 @@ game.SpendGold = Object.extend({
             }
         }
         
+        this.checkBuyKeys();
+        
         return true;
     },
     
@@ -152,5 +154,46 @@ game.SpendGold = Object.extend({
         me.input.unbindKey(me.input.KEY.F5, "F5", true);
         me.input.unbindKey(me.input.KEY.F6, "F6", true);
         me.game.world.removeChild(game.data.buytext);
+    },
+    
+    checkBuyKeys: function() {
+        if(me.input.isKeyPressed("F1")){
+            if(this.checkCost(1)){
+                this.makePurchase(1);
+            }
+        }else if(me.input.isKeyPressed("F2")){
+            if(this.checkCost(2)){
+                this.makePurchase(2);
+            }
+        }else if(me.input.isKeyPressed("F3")){
+            if(this.checkCost(3)){
+                this.makePurchase(3);
+            }
+        }else if(me.input.isKeyPressed("F4")){
+            if(this.checkCost(4)){
+                this.makePurchase(4);
+            }
+        }else if(me.input.isKeyPressed("F5")){
+            if(this.checkCost(5)){
+                this.makePurchase(5);
+            }
+        }else if(me.input.isKeyPressed("F6")){
+            if(this.checkCost(6)){
+                this.makePurchase(6);
+            }
+        }
+    },
+    
+    checkCost: function(skill){
+        if(skill===1 && ( >= game.data.gold)){
+            return true;
+        }else{
+            return false;
+        }
+    },
+    
+    makePurchase: function(skill){
+        
     }
+    
 });
